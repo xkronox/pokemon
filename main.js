0f66ff2f1c5eee51ec9567d3709699baf925924e
kronox <el.kronox@gmail.com>
2023-05-21 22:48:35 -0400

    agregado el img en el js, agregada la imagen

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-const url = 'https://pokeapi.co/api/v2/pokemon/ditto'
+const url = 'https://pokeapi.co/api/v2/pokemon/growlithe'
+const carta = document.querySelector("#card")
 
 function pokemon(){
 
     fetch(url)
-    .then(response => response.json())    
-    .then(data => console.log(data))
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {        
+        console.log(data)
+        carta.innerHTML =
+        `<img src=${data.sprites.other["official-artwork"].front_default}>`
+    })
     .catch(err => console.log(err))
+
+    
 }
 
 pokemon()
+    
